9d600bdeec6cbe8c27f64b6a3edec2e8bc3275a6
Chris Christensen <christian.christensen@gmail.com>
2011-10-12 17:27:03 -0500

    Add WADL XML to "pretty" HTML script

---
+++ scripts/getwadltohtml.sh
@@ -0,0 +1,23 @@
+
+# Dependencies:
+# *  [curl](http://curl.haxx.se/)
+# *  [xsltproc](http://xmlsoft.org/XSLT/xsltproc2.html)
+# *  [tidyp](http://www.tidyp.com/)
+# Note: This is a utility script to pull WADL docs from AllPlayers.com and convert them to pretty HTML (especially useful for copying into the dev. documentation)
+
+# Download WADL and Stylesheet (auth and non-auth)
+curl -k https://www.pdup.allplayers.com/wadl.xsl -o wadl.xsl
+curl -k https://www.pdup.allplayers.com/?q=api/v1/rest/wadl/describe.xml -o describe_nonauth.xml
+curl -k -c cjar -d 'username=USERNAME&password=PASSWORD' https://www.pdup.allplayers.com/api/v1/rest/users/login.json
+curl -k -b cjar https://www.pdup.allplayers.com/?q=api/v1/rest/wadl/describe.xml -o describe_auth.xml
+
+# Convert the WADL XML to HTML
+xsltproc wadl.xsl describe_nonauth.xml > describe_nonauth.html
+xsltproc wadl.xsl describe_auth.xml > describe_auth.html
+
+# Pretty print the HTML
+tidy -imu describe_auth.html
+tidy -imu describe_nonauth.html
+
+# Cleanup temp files
+rm wadl.xsl describe_nonauth.xml describe_auth.xml cjar # describe_nonauth.html describe_auth.html
