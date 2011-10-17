bbbe10d1aedee8a09433dd2a2c8c47c4d83d1d42
Chris Christensen <christian.christensen@gmail.com>
2011-10-16 21:37:42 -0500

    Cleanup js to pass simple lint tests.

--- shared/js/documentation.js
+++ shared/js/documentation.js
@@ -2,74 +2,74 @@
 $(function() {
   var activeItem,
       helpList = $('#js-sidebar .js-topic'),
-      firstOccurance = true
+      firstOccurance = true;
 
   // hide list items at startup
   if($('body.api') && window.location){
     var reg = /\/\/[^\/]+(\/.+)/g,
-        docUrl = reg.exec(window.location.toString())
+        docUrl = reg.exec(window.location.toString());
     if(docUrl){
       $('#js-sidebar .js-topic a').each(function(){
-        var url = $(this).attr('href').toString()
+        var url = $(this).attr('href').toString();
         if(url.indexOf(docUrl[1]) >= 0 && url.length == docUrl[1].length){
-          $(this).parent('li').addClass('disable')
-          var parentTopic = $(this).parentsUntil('div.sidebar-module > ul').last()
-          parentTopic.addClass('js-current')
-          parentTopic.find('.js-expand-btn').toggleClass('collapsed expanded')
+          $(this).parent('li').addClass('disable');
+          var parentTopic = $(this).parentsUntil('div.sidebar-module > ul').last();
+          parentTopic.addClass('js-current');
+          parentTopic.find('.js-expand-btn').toggleClass('collapsed expanded');
         }
-      })
+      });
     }
   }
 
   $('#js-sidebar .js-topic').each(function(){
     if(($(this).find('.disable').length == 0 || firstOccurance == false) &&
     $(this).hasClass('js-current') != true){
-      $(this).find('.js-guides').children().hide()
+      $(this).find('.js-guides').children().hide();
     } else {
-      activeItem = $(this).index()
-      firstOccurance = false
+      activeItem = $(this).index();
+      firstOccurance = false;
     }
-  })
+  });
 
   // Toggle style list. Expanded items stay
   // expanded when new items are clicked.
   $('#js-sidebar .js-toggle-list .js-expand-btn').click(function(){
     var clickedTopic = $(this).parents('.js-topic'),
-        topicGuides  = clickedTopic.find('.js-guides li')
-    $(this).toggleClass('collapsed expanded')
-    topicGuides.toggle(100)
-    return false
-  })
+        topicGuides  = clickedTopic.find('.js-guides li');
+    $(this).toggleClass('collapsed expanded');
+    topicGuides.toggle(100);
+    return false;
+  });
 
   // Accordion style list. Expanded items
   // collapse when new items are clicked.
   $('#js-sidebar .js-accordion-list .js-topic h3 a').click(function(){
     var clickedTopic = $(this).parents('.js-topic'),
-        topicGuides = clickedTopic.find('.js-guides li')
+        topicGuides = clickedTopic.find('.js-guides li');
     
     if(activeItem != clickedTopic.index()){
       if(helpList.eq(activeItem)){
-        helpList.eq(activeItem).find('.js-guides li').toggle(100)
+        helpList.eq(activeItem).find('.js-guides li').toggle(100);
       }
-      activeItem = clickedTopic.index()
-      topicGuides.toggle(100)
+      activeItem = clickedTopic.index();
+      topicGuides.toggle(100);
     } else {
-      activeItem = undefined
-      topicGuides.toggle(100)
+      activeItem = undefined;
+      topicGuides.toggle(100);
     }
 
-    return false
-  })
+    return false;
+  });
 
   $('.help-search .search-box').focus(function(){
-    $(this).css('background-position','0px -25px')
-  })
+    $(this).css('background-position','0px -25px');
+  });
 
   $('.help-search .search-box').focusout(function(){
     if($(this).val() == ''){
-      $(this).css('background-position','0px 0px')
+      $(this).css('background-position','0px 0px');
     }
-  })
+  });
 
   // Dynamic year for footer copyright
   var currentYear = (new Date).getFullYear();
